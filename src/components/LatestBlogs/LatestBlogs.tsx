aa58e7d760ffece9c5318a0abe3fbedfca7346b2
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-09-03 21:58:16 +0600

    added latest blogs components

---
+++ src/components/LatestBlogs/LatestBlogs.tsx
@@ -0,0 +1,16 @@
+const LatestBlogs = () => {
+  return (
+    <div>
+      <h1 className="text-center text-4xl my-5">
+        Latest Blogs from <span className="text-accent">Blogiz</span>
+      </h1>
+      <p className="text-gray-500 text-center">
+        <i>
+          Check out the latest blogs from Blogiz. Stay tuned for more updates.
+        </i>
+      </p>
+    </div>
+  );
+};
+
+export default LatestBlogs;
