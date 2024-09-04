4bd7124a4f1665ec47ff65e256de865543a9db24
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-09-05 01:18:46 +0600

    updated blog details page

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,9 @@
 export interface TBlog {
-  id: string;
+  id: number;
   title: string;
   description: string;
   publish_date: string;
   author_name: string;
   blog_image: string;
-  total_likes: string;
+  total_likes: number;
 }
